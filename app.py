4b160090031641be2f2f3283f9ade725826b0b81
Ricardo Bañuelos De La Torre <47264662+RicardoBanuelos@users.noreply.github.com>
2024-05-25 10:29:19 -0700

    resize image endpoint working

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
-from fastapi import FastAPI, File, UploadFile
 import os
-
+import cv2 
+import glob
+import uuid
 from utils import validate_file_type
+from fastapi import FastAPI, File, UploadFile
+
+import uvicorn
 
 app = FastAPI()
 
 UPLOAD_DIRECTORY = "./uploads"
+RESIZED_DIRECTORY = "./resized_images"
+
 os.makedirs(UPLOAD_DIRECTORY, exist_ok=True)
 
 @app.get("/")
@@ -16,11 +22,45 @@ async def root():
 async def upload_image(file: UploadFile = File(...)):
     validate_file_type(file.file)
     try:
+        _, file_extension = os.path.splitext(file.filename)
+        unique_name = uuid.uuid4()
+
+        file.filename = str(unique_name) + file_extension
+
         file_path = os.path.join(UPLOAD_DIRECTORY, file.filename)
         with open(file_path, "wb") as buffer:
             contents = await file.read()
             buffer.write(contents)
 
-            return {"message" : f"Successfully uploaded {file.filename}"}
+        return {"unique_name" : file.filename}
+    
     except Exception as e:
         return {"error" : str(e)}
+    
+@app.get("/resize_image")
+async def resize_image(width: str, height: str, filename: str):
+
+    if(not width.isdigit() or not height.isdigit()):
+        return {"error" : "invalid width or height"}    
+    
+    new_width = int(width)
+    new_height = int(height)
+
+    if(new_width == 0 or new_height == 0):
+        return {"error" : "invalid dimensions"}
+
+    image_path = os.path.join(UPLOAD_DIRECTORY, filename)
+    image = cv2.imread(cv2.samples.findFile(image_path))
+
+    if(image is None):
+        return {"error" : "failed to open image for resizing"}
+
+    resized_image = cv2.resize(image, (new_width, new_height), interpolation=cv2.INTER_LINEAR)
+
+    resized_image_path = os.path.join(RESIZED_DIRECTORY, filename)
+    cv2.imwrite(resized_image_path, resized_image)
+    
+    return {"message" : "resized image: " + filename}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
