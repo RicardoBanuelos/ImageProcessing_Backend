e9b0bca2874f754e86bfaae2282634b8ddf348d9
Ricardo Bañuelos De La Torre <47264662+RicardoBanuelos@users.noreply.github.com>
2024-07-02 15:14:15 -0700

    added multiple processing endpoints

--- app.py
+++ app.py
@@ -3,16 +3,16 @@ import cv2
 import uuid
 import base64
 
-from utils import validate_file_type
+from utils import validate_file_type, read_image, encodeToBase64
 
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile, HTTPException
 from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
 
 import uvicorn
 
 UPLOAD_DIRECTORY = "./uploads"
-RESIZED_DIRECTORY = "./resized_images"
+PROCESSED_DIRECTORY = "./processed_images"
 
 app = FastAPI()
 
@@ -25,14 +25,15 @@ app.add_middleware(
     allow_headers=["*"],  # Allows all headers
 )
 
+os.makedirs(PROCESSED_DIRECTORY, exist_ok=True)
 os.makedirs(UPLOAD_DIRECTORY, exist_ok=True)
 
 @app.get("/")
 async def root():
     return {"message" : "Hello World"}
 
-@app.post("/upload_image")
-async def upload_image(file: UploadFile = File(...)):
+@app.post("/upload")
+async def upload(file: UploadFile = File(...)):
     validate_file_type(file.file)
     try:
         _, file_extension = os.path.splitext(file.filename)
@@ -48,39 +49,115 @@ async def upload_image(file: UploadFile = File(...)):
         return {"unique_name" : file.filename}
     
     except Exception as e:
-        return {"error" : str(e)}
+        return HTTPException(status_code=500, detail=e)
     
-@app.get("/resize_image")
-async def resize_image(width: str, height: str, filename: str):
+@app.get("/resize")
+async def resize(width: str, height: str, filename: str):
 
     if(not width.isdigit() or not height.isdigit()):
-        return {"error" : "invalid width or height"}    
+        return HTTPException(status_code=400, detail="Invalid dimensions.")    
     
     new_width = int(width)
     new_height = int(height)
 
     if(new_width == 0 or new_height == 0):
-        return {"error" : "invalid dimensions"}
+        return HTTPException(status_code=400, detail="Invalid dimensions.")
 
     image_path = os.path.join(UPLOAD_DIRECTORY, filename)
-    image = cv2.imread(cv2.samples.findFile(image_path))
-
-    if(image is None):
-        return {"error" : "failed to open image for resizing"}
+    image = read_image(image_path)
 
     resized_image = cv2.resize(image, (new_width, new_height), interpolation=cv2.INTER_LINEAR)
 
-    temp_resized_image_path  = os.path.join(RESIZED_DIRECTORY, filename)
+    temp_resized_image_path  = os.path.join(PROCESSED_DIRECTORY, filename)
     cv2.imwrite(temp_resized_image_path , resized_image)
 
-    with open(temp_resized_image_path , 'rb') as image_file:
-        img_data = image_file.read()
-
-    encoded_string = base64.b64encode(img_data).decode('utf-8')
+    encoded_string = encodeToBase64(temp_resized_image_path)
 
     os.remove(temp_resized_image_path)
     os.remove(os.path.join(UPLOAD_DIRECTORY, filename))
     
+    return {"image" : "data:image/[type];base64," + encoded_string}
+
+@app.get("/blur")
+async def blur(kernel_width: str, kernel_height: str, filename: str):
+    if(not kernel_width.isdigit() or not kernel_height.isdigit()):
+        return HTTPException(status_code=400, detail="Invalid dimensions.")    
+    
+    k_width = int(kernel_width)
+    k_height = int(kernel_height)
+
+    image_path = os.path.join(UPLOAD_DIRECTORY, filename)
+    image = read_image(image_path)
+
+    blurred_image = cv2.blur(image, (k_width, k_height))
+    temp_blurred_image_path  = os.path.join(PROCESSED_DIRECTORY, filename)
+    cv2.imwrite(temp_blurred_image_path , blurred_image)
+
+    encoded_string = encodeToBase64(temp_blurred_image_path)
+
+    os.remove(temp_blurred_image_path)
+    os.remove(os.path.join(UPLOAD_DIRECTORY, filename))
+
+    return {"image" : "data:image/[type];base64," + encoded_string}
+
+@app.get("/gaussian_blur")
+async def gaussian_blur(kernel_width: str, kernel_height: str, sigmaX: str, filename: str):
+    if(not kernel_width.isdigit() or not kernel_height.isdigit() or not sigmaX.isdigit()):
+        return HTTPException(status_code=400, detail="Invalid dimensions.")    
+    
+    k_width = int(kernel_width)
+    k_height = int(kernel_height)
+    sigma = int(sigmaX)
+
+    image_path = os.path.join(UPLOAD_DIRECTORY, filename)
+    image = read_image(image_path)
+
+    blurred_image = cv2.GaussianBlur(image, (k_width, k_height), sigma)
+    temp_blurred_image_path  = os.path.join(PROCESSED_DIRECTORY, filename)
+    cv2.imwrite(temp_blurred_image_path , blurred_image)
+
+    encoded_string = encodeToBase64(temp_blurred_image_path)
+
+    os.remove(temp_blurred_image_path)
+    os.remove(os.path.join(UPLOAD_DIRECTORY, filename))
+
+    return {"image" : "data:image/[type];base64," + encoded_string}
+
+@app.get("/median_blur")
+async def median_blur(percentage: str, filename: str):
+    if(not percentage.isdigit()):
+        return HTTPException(status_code=400, detail="Invalid dimensions.")    
+    
+    p = int(percentage)
+
+    image_path = os.path.join(UPLOAD_DIRECTORY, filename)
+    image = read_image(image_path)
+
+    blurred_image = cv2.medianBlur(image, p)
+    temp_blurred_image_path  = os.path.join(PROCESSED_DIRECTORY, filename)
+    cv2.imwrite(temp_blurred_image_path , blurred_image)
+
+    encoded_string = encodeToBase64(temp_blurred_image_path)
+
+    os.remove(temp_blurred_image_path)
+    os.remove(os.path.join(UPLOAD_DIRECTORY, filename))
+
+    return {"image" : "data:image/[type];base64," + encoded_string}
+
+@app.get("/grayscale")
+async def grayscale_image(filename: str):
+    image_path = os.path.join(UPLOAD_DIRECTORY, filename)
+    image = read_image(image_path)
+
+    grayscale_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    temp_greyscale_image_path  = os.path.join(PROCESSED_DIRECTORY, filename)
+    cv2.imwrite(temp_greyscale_image_path , grayscale_image)
+
+    encoded_string = encodeToBase64(temp_greyscale_image_path)
+
+    os.remove(temp_greyscale_image_path)
+    os.remove(os.path.join(UPLOAD_DIRECTORY, filename))
+
     return {"image" : "data:image/[type];base64," + encoded_string}
 
 if __name__ == "__main__":
--- utils.py
+++ utils.py
@@ -2,6 +2,9 @@ from fastapi import HTTPException, status
 from typing import IO
 import filetype
 
+import cv2
+import base64
+
 def validate_file_type(file: IO):
     accepted_mime_types = ["image/png", "image/jpeg", "image/jpg"]
     file_info = filetype.guess(file)
@@ -10,3 +13,17 @@ def validate_file_type(file: IO):
     detected_mime_type = file_info.mime
     if detected_mime_type not in accepted_mime_types:
         raise HTTPException(status_code=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE, detail="Unsupported file type")
+    
+def read_image(image_path: str):
+    image = cv2.imread(cv2.samples.findFile(image_path))
+
+    if(image is None):
+        return HTTPException(status_code=500, detail="Failed reading image.")
+    
+    return image
+
+def encodeToBase64(image_path: str):
+    with open(image_path , 'rb') as image_file:
+        img_data = image_file.read()
+
+    return base64.b64encode(img_data).decode('utf-8')
